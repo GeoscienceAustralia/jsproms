26a315107d2eaf712699bc54993506d7279ebe46
laurams <laurasmes@gmail.com>
2016-06-24 10:59:59 +1000

    Initial commit

---
+++ prov_agent.js
@@ -0,0 +1,25 @@
+/**
+ * Created by u15873 on 17/06/2016.
+ */
+
+// import rdflib
+var rdfstore = require('rdfstore');
+
+
+// create provagent class
+
+
+
+
+
+
+
+
+
+
+// make_graph function
+
+
+
+
+// get_graph function
